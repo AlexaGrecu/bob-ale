3b3a0abc3d123d738e216f474c4889bff27feeb4
AlexaGrecu <alexandra.c459@gmail.com>
2023-02-07 11:03:29 +0200

    new modifications in the homework

---
+++ test_bobita.py
@@ -0,0 +1,6 @@
+dict = {'count': 82,
+ 'next': 'https://swapi.dev/api/people/?page=2',
+ 'previous': None}
+
+next = dict['next']
+print(type(next), next)
--- testing_env.py
+++ testing_env.py
@@ -3,8 +3,6 @@
 import requests
 from pprint import pprint
 
-
-
 # get info from URL -- https://requests.readthedocs.io/en/latest/user/quickstart/
 r_get = requests.get('https://swapi.dev/api/people/',
                      headers={'content-type': 'application/json'})
@@ -16,32 +14,58 @@ r_get = requests.get('https://swapi.dev/api/people/',
 # 4. print only people with eye colour "blue"
 
 # get methods for requests GET object
-print(dir(r_get))
+# print(dir(r_get))
 
 #
 json_data = r_get.json()
 
-pprint(json_data)
-print(type(json_data))
+# print(type(json_data))
+# pprint(json_data)
 
 
-people = list(json_data.values())
-my_people_details_list=(people[3])
-print(my_people_details_list)
+my_people_details_list = dict(json_data)['results']
+# print(my_people_details_list)
 
 print(len(my_people_details_list))
-my_people_with_blue_eyes_list=[]
-i=0
 
-while i<10:
-    if my_people_details_list[i].get('eye_color')=='blue':
-        print('The name of the character with blue eyes is: ', my_people_details_list[i].get('name'))
+my_people_with_blue_eyes_list = []
+
+print('The characters with blue eyes are: ')
+
+i = 0
+while i < 10:
+    if my_people_details_list[i].get('eye_color') == 'blue':
+        # print(my_people_details_list[i].get('name'))
         my_people_with_blue_eyes_list.append(my_people_details_list[i].get('name'))
-
-
     else:
         pass
 
-    i+=1
-print('The characters with blue eyes are:', my_people_with_blue_eyes_list)
-print('The number of people with blue eyes is: ', len(my_people_with_blue_eyes_list))
+    i += 1
+
+# print(my_people_with_blue_eyes_list)
+
+for person in my_people_with_blue_eyes_list:
+    print(person)
+
+# for i in range(10):
+#     if my_people_details_list[i].get('eye_color') == 'blue':
+#         print(my_people_details_list[i].get('name'))
+#         my_people_with_blue_eyes_list.append(my_people_details_list[i].get('name'))
+
+# print('The number of characters with blue eyes is: ', )
+
+result_var = f'''There are {len(my_people_with_blue_eyes_list)} people with Blue eyes. '''
+
+print("There are " + str(len(my_people_with_blue_eyes_list)) + " people with Blue eyes.")
+print(result_var)
+
+# print('The characters with blue eyes are:', my_people_with_blue_eyes_list)
+
+
+# using list "my_people_with_blue_eyes_list"
+# print like this:
+#
+# The characters with blue eyes are:
+# Luke Skywalker
+# Owen Lars
+# Beru Whitesun lars

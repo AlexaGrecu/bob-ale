3bd7bcc45ef15abb6df3f78639069b44d7d24380
AlexaGrecu <alexandra.c459@yahoo.com>
2023-02-17 13:14:12 +0200

    modified GET file

--- GET.py
+++ GET.py
@@ -5,22 +5,37 @@ from pprint import pprint
 
 import requests
 import json
+from requests import exceptions
 
-pprint(requests.__version__)
-pprint(requests.__copyright__)
+# pprint(requests.__version__)
+# pprint(requests.__copyright__)
+#
+# r_get = requests.get('https://swapi.dev/api/planets/3')
+# print(r_get.status_code)
+# print(type(r_get))
+# print(type(r_get.headers))
+# pprint(r_get.headers)
+#
+# data = json.loads(r_get.text)
+# pprint(data)
+# print(r_get.is_redirect)
+#
+# r_get = requests.get('https://swapi.dev/api/people/9')
+# print(r_get.status_code)  # successful request
+# print(r_get.text)
+#
+# dir(r_get)
 
-r_get = requests.get('https://swapi.dev/api/planets/3')
-print(r_get.status_code)
-print(type(r_get))
-print(type(r_get.headers))
-pprint(r_get.headers)
 
-data = json.loads(r_get.text)
-pprint(data)
-print(r_get.is_redirect)
+#Exceptions
 
-r_get = requests.get('https://swapi.dev/api/people/9')
-print(r_get.status_code)  # successful request
-print(r_get.text)
+try:
+    requests.get('https://swapi.dev/api/people/93454')
+except exceptions.ConnectionError:
+    print('Error: Connection error')
 
-dir(r_get)
+
+try: requests.get('https://github.com', timeout=0.001)
+
+except exceptions.ConnectionTimeout:
+    print('Timeout Error')
